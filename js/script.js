b89783a4aa8044b5f0754b8c4d7110d024020af3
Santiago <slponce@fi.uba.ar>
2023-02-28 19:31:58 -0300

    Json modified + request Json file

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ function percentScorePosition(scores,actual_score)
         return (i/scores.length)*100;
     }
 
+function mean(record)
+{
+    
+}
+
+
+var xhr=new XMLHttpRequest();
+
+var records;
+
+xhr.addEventListener("load",(e)=>
+{
+    console.log("listo");
+    console.log(e.target.responseText);
+
+    records=JSON.parse(e.target.responseText);
+});
+
+
+
+
+
+
+
+xhr.open('GET',"https://raw.githubusercontent.com/Santiagoponce/Resuts-Summary/5a3fb474fc4fe3d6784d33294d8e1b23df73785f/data.json",false);
+xhr.send();
